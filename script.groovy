f7d8e3ec2504ea3591feec30a841a9b65688cd1f
sakiRex <62448943+sakiRex@users.noreply.github.com>
2020-06-29 11:16:27 -0400

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,5 +1,11 @@
-def function() {
+def buildApp() {
      echo 'building the application..."
 }
-
+def testApp() {
+     echo 'testing the application..."
+}
+def deployApp() {
+     echo 'deploying the application..."
+     echo "deploying version ${VERSION}"
+}
 return this
