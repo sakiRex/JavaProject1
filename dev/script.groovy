d737621c2253a25b0b3509f09d1a8835a0ee2b94
sakiRex <62448943+sakiRex@users.noreply.github.com>
2020-06-30 14:50:28 -0400

    Create script.groovy

---
+++ dev/script.groovy
@@ -0,0 +1,11 @@
+def buildApp() {
+     echo 'script.groovy: building the application..."
+}
+//def testApp() {
+//     echo 'script.groovy: testing the application..."
+//}
+//def deployApp() {
+//     echo 'script.groovy: deploying the application..."
+//     echo "script.groovy: deploying version ${params.VERSION}"
+//}
+return this
